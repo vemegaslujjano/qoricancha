81d36946a5d3dd206747570b5c6f00c70d42a973
vemegaslujjano <venegaslujjano@gmail.com>
2025-03-20 15:51:15 -0500

    Tu mensaje de commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
-  base: '', 
+  base: 'https://github.com/vemegaslujjano/qori1/', 
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'partials'),
